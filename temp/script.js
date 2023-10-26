9eb295efe1d28c0318266acf6e654cf62df3f4d9
Arved Boetefür <arvedb96@gmx.de>
2023-10-26 19:44:23 +0200

    initial commit of the website

---
+++ temp/script.js
@@ -0,0 +1,10 @@
+function addFieldOnMouseOver(){
+    document.getElementById("add-field").style.backgroundColor = "#274b40";
+    document.getElementById("add-icon").style.color = "#fd5523";
+    
+}
+
+function addFieldOnMouseOut(){
+    document.getElementById("add-field").style.backgroundColor = "#356859";
+    document.getElementById("add-icon").style.color = "#fffbe6";
+}
